b578fc504d3f8b72bd0c8959b93fc01084f9b6d9
Henrik Martinsson <henrik.martinsson@capiostgoran.se>
2023-10-18 15:07:39 +0200

    asd

--- enkel/app.py
+++ enkel/app.py
@@ -1,16 +1,2 @@
 import http.server
-import socketserver
-
-PORT = 80
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-def serve_forever():
-    while True:
-        client, addr = server.accept()
-        Handler(client, addr).handle()
-
-if __name__ == "__main__":
-    server = http.server.HTTPServer(("", PORT), Handler)
-    print("Serving at port", PORT)
-    serve_forever()
+http.server
