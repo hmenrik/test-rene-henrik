262aaadb35e44b6cf4df21641997c8ba6635d2a9
Henrik Martinsson <henrik.martinsson@capiostgoran.se>
2023-10-18 12:32:24 +0200

    new script2

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ import socketserver
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        # Get the visitor's IP address
+        ip_address = self.client_address[0]
+
+        # Create a web page with the visitor's IP address
+        html = """
+        <!DOCTYPE html>
+        <html><head><title>My Web Server</title></head>
+        <body><h1>Visitor IP Address: {}</h1></body></html>
+        """.format(ip_address)
+
+        # Send the web page to the visitor
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Content-Length', len(html))
         self.end_headers()
 
-        self.wfile.write(b'<!DOCTYPE html>')
-        self.wfile.write(b'<html><head><title>My Web Server</title></head>')
-        self.wfile.write(b'<body><h1>Visitor IP Address: {}</h1></body></html>'.format(self.client_address[0].encode()))
+        self.wfile.write(html.encode('utf-8'))
 
-if __name__ == '__app__':
-    server = socketserver.TCPServer(('localhost', 80), MyHandler)
+if __name__ == '__main__':
+    server = socketserver.TCPServer(('', 80), MyHandler)
     server.serve_forever()
