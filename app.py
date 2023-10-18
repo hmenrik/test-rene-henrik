3a3703c0b878a9b160148e9fdc2bb1a4a9e40498
Henrik Martinsson <henrik.martinsson@capiostgoran.se>
2023-10-18 12:22:28 +0200

    new script

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import http.server
 import socketserver
- 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes(self.client_address[0], "utf-8"))
- 
-PORT = 80
- 
-Handler = MyHttpRequestHandler
- 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+
+        self.wfile.write(b'<!DOCTYPE html>')
+        self.wfile.write(b'<html><head><title>My Web Server</title></head>')
+        self.wfile.write(b'<body><h1>Visitor IP Address: {}</h1></body></html>'.format(self.client_address[0].encode()))
+
+if __name__ == '__main__':
+    server = socketserver.TCPServer(('localhost', 80), MyHandler)
+    server.serve_forever()
