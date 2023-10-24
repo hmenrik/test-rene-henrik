c43cb4f837be83b4720879ad18f7a93561ea4e7b
Rene Lambin <rene.lambin@capiostgoran.se>
2023-10-24 22:58:32 +0200

    logga på skrämen

--- test-pwsh/app.ps1
+++ test-pwsh/app.ps1
@@ -15,7 +15,8 @@ Try {
 
         $response = ((($out).Split("`n") | Select-String -Pattern 'x-forwarded-for:') -split ':')[1].Trim()
         # logga i console 
-        #$response
+        $response
+        # svara
         $responseBytes = [System.Text.Encoding]::UTF8.GetBytes($response)
         $c.Response.OutputStream.Write($responseBytes, 0, $responseBytes.Length)
         $c.Response.Close()
