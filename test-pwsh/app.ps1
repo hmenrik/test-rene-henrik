e841c1645c35188c37378dcdcf5c7f7b1ef9efbe
rlambin <99756405+rlambin@users.noreply.github.com>
2023-10-24 15:18:12 +0200

    x-forwarder-for output

--- test-pwsh/app.ps1
+++ test-pwsh/app.ps1
@@ -1,17 +1,17 @@
 $b =  New-Object System.Net.HttpListener
-$b.Prefixes.Add("http://+:8080/")
+$b.Prefixes.Add("http://+:8088/")
 while ($true) {
  $b.Start()
  $c = $b.GetContext()
 
  $c.Request.HttpMethod
  $c.Request.Url
- $c.Request.Headers.ToString()
+ $out =  $c.Request.Headers.ToString()
 
  $c.Response.StatusCode = 200
  $c.Response.ContentType = 'text'
 
- $responseJson = "rene`n"
+ $responseJson = "$($out -match 'x-forwarder-for')"
  $responseBytes = [System.Text.Encoding]::UTF8.GetBytes($responseJson)
  $c.Response.OutputStream.Write($responseBytes, 0, $responseBytes.Length)
  $c.Response.Close()
