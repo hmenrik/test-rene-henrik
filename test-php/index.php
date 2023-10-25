69b4ee5b05d33979b7fc554f6499c472555e36ed
Henrik Martinsson <henrik.martinsson@capiostgoran.se>
2023-10-25 10:29:16 +0200

    php

--- test-php/index.php
+++ test-php/index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 function get_client_ip()
 {
@@ -19,5 +20,39 @@ function get_client_ip()
     // }
 }
 
-print get_client_ip();
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Capio S:t Görans Sjukhus - IPTest</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/css/site.css" />
+</head>
+<body>
+    <header>
+    </header>
+    <div class="container">
+        <main role="main" class="pb-3">
+            
+<div class="jumbotron mt-4">
+    <h1>Din IP-adress är:</h1>
+    <h1 class="display-3">print get_client_ip();</h1>
+</div>
+<blockquote class="blockquote text-center">
+    <p class="mb-0">
+    Even the darkest night will end and the sun will rise
+    </p>
+    <footer class="blockquote-footer">
+    Les Mis&#xE9;rables</footer>
+</blockquote>
+        </main>
+    </div>
+
+
+    
+</body>
+</html>
+
+
 ?>
